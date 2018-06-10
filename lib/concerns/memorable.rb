f09b60245fdee40a21dc6cd3896f521ec088e639
Mary-Kate James <marykatj@gmail.com>
2018-06-10 20:00:04 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,17 @@
 module Memorable
-
+  module ClassMethods
   def reset_all
     self.all.clear
   end
 
   def count
     @@artists.count
+  end
+
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
   end
 
 end
